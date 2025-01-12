8c1e610c6f5a88823583f6740b653e4011cc25e8
Allan Rizza <allanrizza.dev@gmail.com>
2025-01-12 16:18:08 -0300

    32B - Borze

---
+++ 32B-Borze/Program.cs
@@ -0,0 +1,24 @@
+﻿string s = Console.ReadLine();
+string res = "";
+
+for (int i = 0; i <= s.Length-1; i++)
+{
+    if (s[i] == '.')
+    {
+        res += "0";
+    }
+    if (s[i] == '-')
+    {
+        if (s[i+1] == '.')
+        {
+            res += "1";
+        }
+        if (s[i+1] == '-')
+        {
+            res += "2";
+        }
+        i += 1;
+    }
+}
+
+Console.WriteLine(res);
